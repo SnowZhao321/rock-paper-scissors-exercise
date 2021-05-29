eb1a41c65b54ef19a850ea58b39725298b45cad2
SnowZhao321 <64279935+SnowZhao321@users.noreply.github.com>
2021-05-29 15:03:18 -0400

    Update game.py

--- game.py
+++ game.py
@@ -7,8 +7,23 @@ print("Rock, Paper, Scissors, Shoot!")
 
 #print(10, 99, "My message", "another message")
 
-
 user_choice = input("Please choose one of 'rock', 'paper', 'scissors': ")
 
 #print(user_choice)
 print("USER_CHOICE: ", user_choice)
+
+# validate the input such that only if it is one of the expected values
+# ... will we continue with the rest of the program
+# ... otherwise we'll stop the progam before it tries to do anything else
+# ... and we'll ask the user to run the program again
+
+
+#if user_choice = "rock" or "paer" or "scissors": # THIS LINE IS PSEUDOCODE
+# if user_choice == "rock":
+if (user_choice == "rock") or (user_choice == "paper") or (user_choice == "scissors"):
+    print("VALID. KEEP GOING")
+else:
+    print("OOPS, invalid input. Please try again.")
+    exit()
+
+print("THIS IS THE END OF OUR GAME. PLEASE PLAY AGAIN.")
